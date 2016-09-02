c884efd8ebf47a61973cc19ab9032b6f507df9a1
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-09-03 01:56:35 +0530

    The default configuration requires us to set PORT environment variable in production. In this case lets force service hostname to be set as well so things are configurable. Additionally, lets add prod.secret.exs for the time being so that this demo project (should) run just after checkout from github repo.

--- hello_phoenix/config/prod.exs
+++ hello_phoenix/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hello_phoenix, HelloPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: {:system, "SERVICE_HOSTNAME"}, port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
@@ -55,6 +55,8 @@ config :logger, level: :info
 #
 #     config :hello_phoenix, HelloPhoenix.Endpoint, server: true
 #
+
+config :phoenix, :serve_endpoints, true
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
---
+++ hello_phoenix/config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  secret_key_base: "fsdT2LueHiqOMIVvbx1yhvqemXDLvPFmnJDC8DDFLL05YAMDw5ijwfpAs0Bb4tQ1"
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "hello_phoenix_prod",
+  pool_size: 20
